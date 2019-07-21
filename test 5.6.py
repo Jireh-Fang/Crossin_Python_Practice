d0ab8bbe43c852e4e0cfab336a92f7e33b5493f4
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-07-21 12:35:35 +0800

    guess_number

--- test 5.6.py
+++ test 5.6.py
@@ -5,6 +5,11 @@ Created on Wed Jul 17 22:46:31 2019
 @author: Administrator
 """
 
+'''
+猜字游戏：程序随机一个结果，用户通过命令行输入去猜。程序会告诉你猜大了还是小了，知道猜中为止。
+附加功能：1.游戏可以反复进行，猜中了之后可以重新开始；2.统计用户猜了几轮，平均几次猜中；
+3.限制每轮猜的次数，判定输赢。
+'''
 from random  import randint
 total = 0
 times = 0
@@ -13,8 +18,13 @@ while True:
     if str(choice) == '1':
         total += 1
         num = randint(1,20)
+        this_time = 0
         while True:
             times += 1
+            this_time += 1
+            if this_time >= 6:
+                print('本轮你已猜5次，失败！')
+                break
             guess = int(input("请输入你要猜的数："))
             if guess > num:
                 print("太大了")
